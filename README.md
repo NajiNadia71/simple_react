8174d24b136ec2a4d6107d177077755343ee0394
Nadia <nadia.ni71@gmail.com>
2025-05-05 06:56:52 +0200

    SimpleMUI

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simple_react
 a simple React project
+
+Run 
+npm run dev after npm install
+there is a recording if just want to see it
--- basic-react-sample/src/main.tsx
+++ basic-react-sample/src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import App from './App';
+import DashboardPage from './pages';
+import OrdersPage from './pages/orders';
+import Layout from './layouts/dashboard';
+import DataGridDemo from './pages/DataGridDemo';
+import StackBars from './pages/StackBars';
+import CustomizedSteppers from './pages/CustomizedSteppers';
+const router = createBrowserRouter([
+  {
+    Component: App, // root layout route
+    children: [
+      {
+        path: '/',
+        Component: Layout,
+        children: [
+          {
+            path: '',
+            Component: DashboardPage,
+          },
+          {
+            path: 'orders',
+            Component: OrdersPage,
+          },
+          {
+            path: 'data-grid',
+            Component:DataGridDemo ,
+          },
+          {
+            path: 'sample-stepper',
+            Component:CustomizedSteppers ,
+          }
+          ,
+          {
+            path: 'charts',
+            Component:StackBars ,
+          },
+          ],
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
